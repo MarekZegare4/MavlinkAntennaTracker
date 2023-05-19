3dac31967548bdf59bd0f4b210eda5e6eae61e11
Jakub Siciarek <jakub@MacBook-Air-Jakub-2.local>
2023-05-19 19:10:46 +0200

    Nowe srodowsiko

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from pymavlink import mavutil
+import signal
 
-# Start a connection listening on a UDP port
-the_connection = mavutil.mavlink_connection('udpin:localhost:14540')
+def handler(signum, frame):
+    print("Forever is over!")
+    raise Exception("end of time")
 
-# Wait for the first heartbeat 
-# This sets the system and component ID of remote system for the link
-the_connection.wait_heartbeat()
-print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
+def loop_forever():
+    import time
+    while 1:
+        print("sec")
+        time.sleep(1)
 
-# Once connected, use 'the_connection' to get and send messages
+signal.signal(signal.SIGALRM, handler)
+signal.alarm(10)
+try:
+    loop_forever()
+except Exception, exc: 
+    print(exc) 
 
-try: 
-    altitude = the_connection.messages['GPS_RAW_INT'].alt  # Note, you can access message fields as attributes!
-    timestamp = the_connection.time_since('GPS_RAW_INT')
-except:
-    print('No GPS_RAW_INT message received')
