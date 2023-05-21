daa979fc38c886d6bf79e27053080262f39d7385
MarekZegare4 <106778416+MarekZegare4@users.noreply.github.com>
2023-05-21 17:24:50 +0200

    Dodane menu z wyborem portów

--- MAV_connect.py
+++ MAV_connect.py
@@ -13,6 +13,7 @@
 
 from pymavlink import mavutil
 import serial.tools.list_ports
+from pick import pick
 baudRate = 9600
 
 def wait_heartbeat(m):
@@ -28,19 +29,23 @@ ports = serial.tools.list_ports.comports()
 def connect_serial(baudRate):
     ports = serial.tools.list_ports.comports()
     com_list = []
+    name_list = []
     for p in ports:
-        print(p)
         com_list.append(p.device)
+        name_list.append(p.description)
     if len(com_list) == 0:
         print('No serial devices connected')
         return 0
     else:
-        which_serial = int(input("Choose from 1-" + str(len(com_list))+" -> "))
-        adress = str(com_list[which_serial - 1])
+        title = 'Select port'
+        option, index = pick(name_list, title)
+        print(index)
+        adress = str(com_list[index])
         master = mavutil.mavlink_connection(adress, baud=baudRate)
+        print(master)
         wait_heartbeat(master)
         print("Sending all message types")
-        return 
+        return 1
 
 connect_serial(baudRate)
 
